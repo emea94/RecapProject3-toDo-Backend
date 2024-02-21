6876150ff7a37be13a6e3affb5d1c72bafb66d1c
emea94 <emea94@web.de>
2024-02-21 15:59:38 +0100

    Setup Project

---
+++ src/main/java/com/example/recapproject3todobackend/RecapProject3ToDoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recapproject3todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProject3ToDoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapProject3ToDoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recapproject3todobackend/RecapProject3ToDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recapproject3todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProject3ToDoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
